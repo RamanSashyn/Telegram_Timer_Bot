598fc0d39cb8c4967405e00596c01b299e638e0a
Raman <149716575+RamanSashyn@users.noreply.github.com>
2024-12-05 18:17:12 +0100

    Отправка сообщения: Осталось Х секунд; Время вышло!

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import ptbot
-
 from dotenv import load_dotenv
 import os
-
-import random
+from pytimeparse import parse
 
 load_dotenv()
 
@@ -11,15 +9,33 @@ tg_token = os.getenv("TG_TOKEN")
 TG_CHAT_ID = "571873439"
 bot = ptbot.Bot(tg_token)
 
-def wait(chat_id, question):
-    bot.create_timer(5, choose, chat_id=chat_id, question=question)
 
-def choose(chat_id, question):
-    message = "Время вышло!".format(question)
+def notify_progress(secs_left, chat_id):
+    if secs_left > 0:
+        message = f"Осталось {secs_left} секунд"
+        bot.send_message(chat_id, message)
+    elif secs_left == 0:
+        bot.send_message(chat_id, "Осталось 0 секунд")
+        bot.create_timer(0.001, choose, chat_id=chat_id)
+
+
+
+def choose(chat_id):
+    message = "Время вышло!"
     bot.send_message(chat_id, message)
-    print("Мне написал пользователь с ID:", chat_id)
-    print("Он спрашивал:", question)
-    print("Я ответил:", message)
+
+
+
+def wait(chat_id, question):
+
+    seconds_left = parse(question)
+
+    if seconds_left:
+        bot.create_countdown(seconds_left, notify_progress, chat_id=chat_id)
+
+
+    else:
+        bot.send_message(chat_id, "Неверный формат времени. Используйте, например, '5s' или '10m'.")
 
 bot = ptbot.Bot(tg_token)
 bot.reply_on_message(wait)
