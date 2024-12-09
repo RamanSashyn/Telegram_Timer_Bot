838b35726089bff1c10576000f4219bc3d250e07
Raman <149716575+RamanSashyn@users.noreply.github.com>
2024-12-09 14:45:50 +0100

    Отправка с обновлением сообщения: Осталось Х секунд; Время вышло!

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ tg_token = os.getenv("TG_TOKEN")
 TG_CHAT_ID = "571873439"
 bot = ptbot.Bot(tg_token)
 
+last_message_text = None
 
-def notify_progress(secs_left, chat_id):
+
+def notify_progress(secs_left, chat_id, message_id):
     if secs_left > 0:
         message = f"Осталось {secs_left} секунд"
-        bot.send_message(chat_id, message)
+        bot.update_message(chat_id, message_id, message)
     elif secs_left == 0:
-        bot.send_message(chat_id, "Осталось 0 секунд")
+        bot.update_message(chat_id, message_id, "Осталось 0 секунд")
         bot.create_timer(0.001, choose, chat_id=chat_id)
-
 
 
 def choose(chat_id):
@@ -31,9 +32,9 @@ def wait(chat_id, question):
     seconds_left = parse(question)
 
     if seconds_left:
-        bot.create_countdown(seconds_left, notify_progress, chat_id=chat_id)
-
-
+        message = bot.send_message(chat_id, f"Осталось {seconds_left} секунд")
+        message_id = message
+        bot.create_countdown(seconds_left, notify_progress, chat_id=chat_id, message_id=message_id)
     else:
         bot.send_message(chat_id, "Неверный формат времени. Используйте, например, '5s' или '10m'.")
 
